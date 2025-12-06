97e0410418ed20dd2a09f7178867013f45153b79
yoichi-kaneko <kaneko@webstudio-wanderlust.voyage>
2025-12-06 11:35:36 +0900

    Csrfチェックの除外、細かい調整

--- app/Http/Controllers/SlacksController.php
+++ app/Http/Controllers/SlacksController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
-use App\Jobs\RelayCli;
 use App\Services\Slack\SlackEventService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\SlacksController;
+use App\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::post('/slack/events', [SlacksController::class, 'event'])
+    ->withoutMiddleware(VerifyCsrfToken::class)
     ->middleware('slack.signature');
